af416df34055d0d781589b9599a5c501eec73728
btrosso <51491995+btrosso@users.noreply.github.com>
2022-04-16 22:47:30 -0500

    update main.py & contact.html
    fleshed out the functionality behind the contact.html page

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
+import smtplib
 import requests
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 load_dotenv()
 
 app = Flask(__name__)
@@ -18,9 +20,34 @@ def home_page():
 def about_page():
     return render_template("about.html")
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact_page():
-    return render_template("contact.html")
+    if request.method == 'POST':
+        name = request.form["name"]
+        email = request.form["email"]
+        phone_number = request.form["phoneNumber"]
+        message = request.form["message"]
+        success_message = "Successfully sent your message"
+
+        # send the message via smtp
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=os.environ["MY_EMAIL"], password=os.environ["EMAIL_PASSWORD"])
+            connection.sendmail(
+                from_addr=os.environ["MY_EMAIL"],
+                to_addrs="riseandgrind2020@yahoo.com",
+                msg="Subject:New Message\n\n"
+                    f"Name: {name}\n"
+                    f"Email:{email}\n"
+                    f"Phone Number:{phone_number}\n"
+                    f"Message:{message}")
+        print(name)
+        print(email)
+        print(phone_number)
+        print(message)
+        return render_template("contact.html", success_message=success_message)
+    else:
+        return render_template("contact.html")
 
 @app.route("/post/<int:index>")
 def show_post(index):
@@ -29,6 +56,7 @@ def show_post(index):
         if blog_post["id"] == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
